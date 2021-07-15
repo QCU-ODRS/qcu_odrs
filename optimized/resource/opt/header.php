2485f600e5585f030b9836bc68e2c53c4ebfe6b5
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-15 18:22:42 +0800

    Update header.php

--- optimized/resource/opt/header.php
+++ optimized/resource/opt/header.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CssJs/tableview.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
     <title>Documents</title>
   </head>
