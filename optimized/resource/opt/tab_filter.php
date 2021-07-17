a359a2a38e6d9b0916f078fc10e4485cbf4530ae
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-18 00:12:14 +0800

    Update tab_filter.php

---
+++ optimized/resource/opt/tab_filter.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <button class="btn btn-primary" onclick="filterSelection('Requests List')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('Pending List')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('In-Process List')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('Archived List')"></button>
+</div>
