0f492e3ab95a01f15399434844eb20186dbdb106
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-17 13:13:48 +0800

    Update pendinglist_table.php

--- optimized/resource/opt/pendinglist_table.php
+++ optimized/resource/opt/pendinglist_table.php
@@ -1,11 +1,11 @@
 <table>
     <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th>Request Number</th>
+        <th>Date Requested</th>
+        <th>Requester's Name</th>
+        <th>Document Requested</th>
+        <th>File Viewer</th>
+        <th>Action</th>
     </tr>
     <tr>
         <td></td>
