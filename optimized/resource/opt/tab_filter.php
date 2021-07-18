385c16cc5a8b911ba2d01d2cecc829de7aacc61a
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-19 01:03:22 +0800

    Update tab_filter.php

--- optimized/resource/opt/tab_filter.php
+++ optimized/resource/opt/tab_filter.php
@@ -1,6 +1,20 @@
 <div class="container">
-    <button class="btn btn-primary" onclick="filterSelection('Requests List')"></button>
-    <button class="btn btn-primary" onclick="filterSelection('Pending List')"></button>
-    <button class="btn btn-primary" onclick="filterSelection('In-Process List')"></button>
-    <button class="btn btn-primary" onclick="filterSelection('Archived List')"></button>
+<button class="btn btn-primary" onclick="filterSelection('ShowAll')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('RequestsList')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('PendingList')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('In-ProcessList')"></button>
+    <button class="btn btn-primary" onclick="filterSelection('ArchivedList')"></button>
+</div>
+<div class="container">
+    <div class="d-sm-flex">
+        <div class="flex-sm-column" id="myBtnContainer">
+            <div class="column RequestsList">
+                <div class="content">
+                    <?php
+                        include "RequestsList.php";
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
