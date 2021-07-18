1c7e4fb2a1df59767fa691c40f637c5fc4e21fb0
grey-xiao <87128634+grey-xiao@users.noreply.github.com>
2021-07-18 20:06:22 +0800

    Update header.php

--- optimized/resource/opt/header.php
+++ optimized/resource/opt/header.php
@@ -6,13 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="../../../resource/CssJs/tableview.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../CssJs/tableview.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-      <?php
-        include "../CssJs/tableview.css";
-      ?>
-    </style>
+    
     <title>Documents</title>
   </head>
   <body>
