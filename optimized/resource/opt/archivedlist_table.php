935fcae95440ca53a8a8affcb3162eeb08806f10
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-19 16:40:25 +0800

    Update archivedlist_table.php

--- optimized/resource/opt/archivedlist_table.php
+++ optimized/resource/opt/archivedlist_table.php
@@ -1,1 +1,20 @@
-<p>dump</p>
+<table>
+    <tr class="bg-dark" style="color: white;">
+        <th>Document ID</th>
+        <th>Requester's Name</th>
+        <th>Request Details</th>
+        <th>Document Requesting</th>
+        <th>Status</th>
+        <th>Release/Claim Date</th>
+        <th>Action</th>
+    </tr>
+    <tr>
+        <td>asd</td>
+        <td>asd</td>
+        <td>asd</td>
+        <td>asd</td>
+        <td>asd</td>
+        <td>asd</td>
+        <td>asd</td>
+    </tr>
+</table>
