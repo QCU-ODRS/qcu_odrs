38ed38b92b033f0869bfa9cebfb8111dfc517bac
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-19 16:41:33 +0800

    Update tab_filter.php

--- optimized/resource/opt/tab_filter.php
+++ optimized/resource/opt/tab_filter.php
@@ -45,7 +45,7 @@
     function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("column");
-    if (c == "all") c = "";
+    if (c == "ShowAll") c = "";
     for (i = 0; i < x.length; i++) {
         w3RemoveClass(x[i], "show");
         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
