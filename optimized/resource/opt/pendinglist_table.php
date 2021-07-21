6b2c3f8515276f93428c08b352f35b58e2d14102
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-21 15:33:47 +0800

    Update pendinglist_table.php

--- optimized/resource/opt/pendinglist_table.php
+++ optimized/resource/opt/pendinglist_table.php
@@ -3,10 +3,10 @@
 
     $search = $_GET['search'] ?? '';
     if($search) {
-        $statement = $pdo->prepare('SELECT * FROM documents_request WHERE request_status="pending" LIKE :title ORDER BY request_number ASC');
+        $statement = $pdo->prepare('SELECT * FROM document_request WHERE request_status="pending" LIKE :title ORDER BY request_number ASC');
         $statement->bindValue(':title', "%$search%");
     } else {
-        $statement = $pdo->prepare('SELECT * FROM documents_request ORDER BY request_number ASC');
+        $statement = $pdo->prepare('SELECT * FROM document_request ORDER BY request_number ASC');
     }
     $statement->execute();
     $pending_request = $statement->fetch(PDO::FETCH_ASSOC);
@@ -17,7 +17,7 @@
         <tr class="bg-dark" style="color: white;">
             <th scope="col">Request Number</th>
             <th scope="col">Date Requested</th>
-            <th scope="col">Requester's Name</th>
+            <th scope="col">Student Number</th>
             <th scope="col">Document Requested</th>
             <th scope="col">File Viewer</th>
             <th scope="col">Action</th>
@@ -45,7 +45,7 @@
             </td>
             <td>
                 <?php
-                    echo $request['request_number']
+                    echo $request['student_number']
                 ?>
             </td>
             <td>
