8710bdb3b95b7c5fef27180e95a125f02b2e6b27
grey-xiao <87128634+grey-xiao@users.noreply.github.com>
2021-07-23 03:37:21 +0800

    refresh

--- optimized/resource/opt2/header.php
+++ optimized/resource/opt2/header.php
@@ -13,3 +13,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src = "../../resource/CssJs/script.js"> 
     </script>
+    <?php
+    $url1=$_SERVER['REQUEST_URI'];
+    header("Refresh: 5; URL=$url1");
+?>
