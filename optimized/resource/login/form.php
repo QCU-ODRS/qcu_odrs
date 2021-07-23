855732c74c91eb7bcc637606711599ab2a51f2a6
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-23 10:59:18 +0800

    Update form.php

--- optimized/resource/login/form.php
+++ optimized/resource/login/form.php
@@ -8,7 +8,7 @@
         </div>
     <?php endif ?>
 <form action="" method="post" style="position: absolute; width: 300px; height: 425px; left: 850px; top: 275px; bottom: 200px; filter: drop-shadow(10px 10px 2px rgba(0, 0, 0, 0.25)); background-color: #87CEEB;">
-  <h1 style="text-align: center;">LOGIN</h1>
+  <h1 style="text-align: center; font-size: 65px; position: absolute; top: 15px; left: 55px;">LOGIN</h1>
   <div class="mb-3">
     <label class="form-label" style="position: absolute; left: 20px; top: 120px;">Student Number</label>
     <input type="text" class="form-control" name="student_number" style="position: absolute; width: 90%; left: 15px; top: 150px;">
@@ -23,6 +23,6 @@
     <label for="form-label" style="position: absolute; left: 40px; bottom: 105px;">Show Password</label>
   </div>
   <button type="submit" class="btn btn-primary" name="login" style="position: absolute; width: 125px; left: 90px; bottom: 20px;">Submit</button>
-  <a href="register.php" style="position: absolute; width: 125px; left: 225px; top: 210px;">Register</a>
+  <a href="register.php" style="position: absolute; width: 125px; left: 225px; top: 210px; color: black;">Register</a>
 </form>
 <br>
