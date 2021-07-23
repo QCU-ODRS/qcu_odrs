b7dceaa987fcb631bcc33e8efb0377b5776b9079
Archraiche <rhoshielamielibuna@outlook.com>
2021-07-23 10:44:30 +0800

    Update form.php

--- optimized/resource/login/form.php
+++ optimized/resource/login/form.php
@@ -7,21 +7,21 @@
             <?php endforeach?>
         </div>
     <?php endif ?>
-<form action="" method="post" style="position: absolute; width: 300px; height: 400px; left: 850px; top: 275px; bottom: 200px; filter: drop-shadow(10px 10px 2px rgba(0, 0, 0, 0.25)); background-color: #87CEEB;">
+<form action="" method="post" style="position: absolute; width: 300px; height: 425px; left: 850px; top: 275px; bottom: 200px; filter: drop-shadow(10px 10px 2px rgba(0, 0, 0, 0.25)); background-color: #87CEEB;">
   <div class="mb-3">
-    <label class="form-label">Student Number</label>
-    <input type="text" class="form-control" name="student_number">
+    <label class="form-label" style="position: absolute; left: 20px; top: 120px;">Student Number</label>
+    <input type="text" class="form-control" name="student_number" style="position: absolute; width: 90%; left: 15px; top: 150px;">
     <div class="form-text"></div>
   </div>
   <div class="mb-3">
-    <label class="form-label" style="position: absolute; left: 20px; top: 180px;">Password</label>
-    <input type="password" class="form-control" name="acc_pass" id="acc_pass" style="position: absolute; width: 90%; left: 15px; top: 210px;">
+    <label class="form-label" style="position: absolute; left: 20px; top: 210px;">Password</label>
+    <input type="password" class="form-control" name="acc_pass" id="acc_pass" style="position: absolute; width: 90%; left: 15px; top: 240px;">
   </div>
   <div class="mb-3">
-    <input type="checkbox" onclick="toggle_pass()" style="position: absolute; left: 20px; bottom: 130px;">&nbsp;
-    <label for="form-label" style="position: absolute; left: 40px; bottom: 125px;">Show Password</label>
+    <input type="checkbox" onclick="toggle_pass()" style="position: absolute; left: 20px; bottom: 110px;">&nbsp;
+    <label for="form-label" style="position: absolute; left: 40px; bottom: 105px;">Show Password</label>
   </div>
   <button type="submit" class="btn btn-primary" name="login" style="position: absolute; width: 125px; left: 90px; bottom: 20px;">Submit</button>
+  <a href="register.php" >Register</a>
 </form>
 <br>
-<a href="register.php">Register</a>
